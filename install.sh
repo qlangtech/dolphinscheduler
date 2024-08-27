929f70df63c73e93827c51c0b7b7176a792aa6b4
mozhenghua <baisui@qlangtech.com>
2024-08-27 23:29:12 +0800

    remove plugin impl

--- install.sh
+++ install.sh
@@ -1,1 +1,2 @@
-mvn deploy -Ptis-repo -pl dolphinscheduler-api -am -Dmaven.test.skip=true -Dmaven.javadoc.skip=true -Dspotless.check.skip=true  -Dmaven-remote-resources-plugin.skip=true
+mvn deploy -Ptis-repo -pl dolphinscheduler-api,dolphinscheduler-bom \
+ -am -Dmaven.test.skip=true -Dmaven.javadoc.skip=true -Dspotless.check.skip=true  -Dmaven-remote-resources-plugin.skip=true
