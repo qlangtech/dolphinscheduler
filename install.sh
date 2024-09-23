db363cb3a89a8edc99f9011a4540cac8a4b271b8
mozhenghua <baisui@qlangtech.com>
2024-09-23 13:35:56 +0800

    change maven execute phase to deploy

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
-mvn install -Ptis-repo -pl dolphinscheduler-api,dolphinscheduler-bom \
+mvn deploy -Ptis-repo -pl dolphinscheduler-api,dolphinscheduler-bom \
  -am -Dmaven.test.skip=true -Dmaven.javadoc.skip=true -Dspotless.check.skip=true  -Dmaven-remote-resources-plugin.skip=true
